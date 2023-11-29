9b3f8331aad6360c65449c1eb9e6380d531c6c5f
ThomasHuynh <Thomas_TTH@live.com>
2023-11-29 02:21:09 -0500

    Created and styled Navbar components with individual Link components for each page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import Navbar from "@/scenes/navbar";
+import { useState } from "react";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState<string>("benefits");
 
   return (
-    <div className="app">
-
+    <div className="app bg-gray-50">
+      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
